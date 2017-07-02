d6b360253dd17b98d65681fe6e0243b2e5d105e7
Roman <r.efimov@list.ru>
2017-07-02 10:10:45 +0300

    ыфв

--- site-visiting-studio/form/regform.php
+++ site-visiting-studio/form/regform.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'>
-	<meta http-equiv="refresh" content="5; url=http://rabbitweb.ru">
+	<meta http-equiv="refresh" content="1; url=http://rabbitweb.ru">
 	<title>Отправка формы запроса</title>
 </head>
 <body>
@@ -16,6 +16,12 @@
 		$services_2 = $_GET['order2'];
 		$services_3 = $_GET['order3'];
 		$services_4 = $_GET['order4'];
+		$services_5 = $_GET['order5'];
+		$services_6 = $_GET['order6'];
+		$services_7 = $_GET['order7'];
+		$services_8 = $_GET['order8'];
+		$services_9 = $_GET['order9'];
+		$services_10 = $_GET['order10'];
 
 		$to = 'office@rabbitweb.ru';
 		$subject = 'Сообщение с сайта';
@@ -26,18 +32,14 @@
 			"Создать сайт: $services_1\n".
 			"(Re:)Дизайн: $services_2\n".
 			"Продвижение: $services_3\n".
-			"Доп. услуги: $services_4";
+			"Доп. услуги: $services_4\n".
+			"Сайт-визитка: $services_5\n".
+			"Промо: $services_6\n".
+			"Сайт каталог: $services_7\n".
+			"Landing: $services_8\n".
+			"Корпоративный сайт: $services_9\n".
+			"Интернет-магазин: $services_10";
 		mail($to, $subject, $msg, 'From: ' . $email);
-
-		echo '<div id="seo-basic"> <h1>Спасибо за заполнение формы.</h1>';
-		echo '<p>Контактное лицо: '.$name.'</p>';
-		echo '<p>Номер телефона: '.$phone.'</p>';
-		echo '<p>Электронная почта: '.$email.'</p>';
-		echo '<p>Город: '.$city.'</p>';
-		echo '<p>Создать сайт: ' . $services_1 . '</p>';
-		echo '<p>(Re:)Дизайн: ' . $services_2 . '</p>';
-		echo '<p>Продвижение: ' . $services_3 . '</p>';
-		echo '<p>Доп. услуги: ' . $services_4 . '</p> </div>';
 	?>
 </body>
 </html>
