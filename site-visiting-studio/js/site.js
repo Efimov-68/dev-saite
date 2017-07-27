f92ae63207c8813dad09194bfa6de4a465260b7e
Efimov-68 <r.efimov@list.ru>
2017-07-27 20:08:49 +0300

    scratch left panel

--- site-visiting-studio/js/site.js
+++ site-visiting-studio/js/site.js
@@ -23,3 +23,28 @@ $(document).ready(function() {
 		}
 	});
 }); // end ready
+$(document).ready(function() {
+	// open external links in new window
+	$('a[href^="http://"]').attr('target','_blank');
+	
+	//position resources panel on screen
+	$('#resources-scratch').show().delay(500).animate({ left : '-25em' }, 250).animate({left:'-29em'},250);
+	
+	// resources panel
+	var panel=false;
+	$('.open').click(function() {
+		if (!panel) {
+			$(this).text('-');
+			$('#resources-scratch').animate({
+				left : 0
+			}, 250); // end animate
+			panel=true;
+		} else {
+			$(this).text('+');
+			$('#resources-scratch').animate({
+				left : '-29em'
+			}, 250); // end animate
+			panel=false;
+		}
+	});
+}); // end ready
