a5cea3909b07a1cd9eac4c1c0fd96cdd8daf9951
Efimov-68 <r.efimov@list.ru>
2017-07-30 16:34:33 +0300

    ссв

--- site-visiting-studio/js/site.js
+++ site-visiting-studio/js/site.js
@@ -9,13 +9,13 @@ $(document).ready(function() {
 	var panel=false;
 	$('.open').click(function() {
 		if (!panel) {
-			$(this).text('-');
+			$(this).text('x');
 			$('#resources').animate({
 				left : 0
 			}, 250); // end animate
 			panel=true;
 		} else {
-			$(this).text('+');
+			$(this).text('o');
 			$('#resources').animate({
 				left : '-29em'
 			}, 250); // end animate
@@ -34,13 +34,13 @@ $(document).ready(function() {
 	var panel=false;
 	$('.open').click(function() {
 		if (!panel) {
-			$(this).text('-');
+			$(this).text('x');
 			$('#resources-scratch').animate({
 				left : 0
 			}, 250); // end animate
 			panel=true;
 		} else {
-			$(this).text('+');
+			$(this).text('o');
 			$('#resources-scratch').animate({
 				left : '-29em'
 			}, 250); // end animate
@@ -59,13 +59,13 @@ $(document).ready(function() {
 	var panel=false;
 	$('.open').click(function() {
 		if (!panel) {
-			$(this).text('-');
+			$(this).text('x');
 			$('#resources-python').animate({
 				left : 0
 			}, 250); // end animate
 			panel=true;
 		} else {
-			$(this).text('+');
+			$(this).text('o');
 			$('#resources-python').animate({
 				left : '-29em'
 			}, 250); // end animate
@@ -73,3 +73,50 @@ $(document).ready(function() {
 		}
 	});
 }); // end ready
+$(document).ready(function(){
+	$('#info-scratch-learn').hide();
+	$('#info-python-easy').hide();
+	$('#info-python-middle').hide();
+	$('#info-javascript-middle').hide();
+	//end .hide
+	$('#scratch-learn').click(function(){
+    	var $scratchLearn = $(this).next('#info-scratch-learn');
+    	if ($scratchLearn.is(':hidden')) {
+    		$scratchLearn.slideDown(300);
+    		$(this).addClass('close');
+    	}else{
+    		$scratchLearn.slideUp(300);
+    		$(this).removeClass('close');
+    	}
+	});
+	$('#python-easy').click(function(){
+    	var $pythonEasy = $(this).next('#info-python-easy');
+    	if ($pythonEasy.is(':hidden')) {
+    		$pythonEasy.slideDown(300);
+    		$(this).addClass('close');
+    	}else{
+    		$pythonEasy.slideUp(300);
+    		$(this).removeClass('close');
+    	}
+	});
+	$('#python-middle').click(function(){
+    	var $pythonMiddle = $(this).next('#info-python-middle');
+    	if ($pythonMiddle.is(':hidden')) {
+    		$pythonMiddle.slideDown(300);
+    		$(this).addClass('close');
+    	}else{
+    		$pythonMiddle.slideUp(300);
+    		$(this).removeClass('close');
+    	}
+	});
+	$('#javascript-middle').click(function(){
+    	var $javascriptMiddle = $(this).next('#info-javascript-middle');
+    	if ($javascriptMiddle.is(':hidden')) {
+    		$javascriptMiddle.slideDown(300);
+    		$(this).addClass('close');
+    	}else{
+    		$javascriptMiddle.slideUp(300);
+    		$(this).removeClass('close');
+    	}
+	});
+});//end ready
