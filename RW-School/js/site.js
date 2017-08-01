b44eb2fc3640645d4e80a700dc1ab34a37e7cba3
Roman <r.efimov@list.ru>
2017-08-01 20:00:41 +0300

    ljk

--- RW-School/js/site.js
+++ RW-School/js/site.js
@@ -164,3 +164,41 @@ $(document).ready(function(){
     	}
 	});
 });//end ready
+//*******TIME********
+$(document).ready(function() {
+	function displayTime() {
+		$('#time').text(getTime(true));
+	}
+	displayTime();
+	setInterval(displayTime,1000);
+}); // end ready
+function getTime(secs) {
+	var sep = ':'; //separator character
+	var hours,minutes,seconds,time/*,am_pm*/;
+	var now = new Date();
+	hours = now.getHours();
+	/*if (hours < 24) {
+		am_pm = 'am';
+	} else {
+		am_pm = 'pm';
+	}*/
+	hours = hours % 24;
+	if (hours === 0) {
+		hours = 24;
+	} 
+	time = hours;
+	minutes = now.getMinutes();
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	}
+	time += sep + minutes;
+	if (secs) {
+		seconds = now.getSeconds();
+		if (seconds < 10) {
+			seconds = '0' + seconds;
+		}
+		time += sep + seconds;
+	} 
+	return time /*+ ' ' + am_pm*/;
+}
+//*******end TIME********
