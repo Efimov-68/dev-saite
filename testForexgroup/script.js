5ed345b80be9f64acf1da67429969464b1f4f79b
Efimov-68 <r.efimov@list.ru>
2017-08-10 13:25:53 +0300

    Check + del

--- testForexgroup/script.js
+++ testForexgroup/script.js
@@ -1,40 +1,22 @@
 $(document).ready(function(){
-	//$('#data-size td input').hide();
-	//end hide()
-	/*$('#data-size td').click(function(){
-    	var $dataSize = $(this).show('#data-size td input');
-    	if ($dataSize.is(':hidden')) {
-    		$dataSize.slideDown(300);
-    		$(this).addClass('close');
-    	}else{
-    		$dataSize.slideUp(300);
-    		$(this).removeClass('close');
-    	}
-	});*/
 
-	$('#confirm').dialog({
-    modal: true,
-  	autoOpen: false,
-    buttons : {
-      "Подтвердить" : function() {
-        $('#data-size input["checked"]', true).effect('explode');
-        $(this).dialog('close');
-      },
-      "Отменить" : function() {
-        $(this).dialog('close');
-      }
-    } // end buttons
-  });
-  $('#delete input[type="submit"]').click(function() {
-  	$('#confirm').dialog('open');
-  }); // end click
-
+  //Allcontents checked
   $('#allcontents input[name="allcontents"]').click(function(){
-  	if () {
-  		$("input[type=checkbox]").prop('checked', true);
-  		$(this).addClass('.checkInput')
-  	}else{
-  		$("input[type=checkbox]").prop(false);
-  	}
-  });
+      var $allcheck = $('#allcontents input[name="allcontents"]').prop('checked');
+      if ($allcheck == true) {
+        $("input[type=checkbox]").prop('checked', true);
+    		$(this).addClass('.checkInput');
+      }else{
+  		  $("input[type=checkbox]").prop('checked', false);
+        $(this).removeClass('.checkInput');
+      }
+  });//end All checked
+  //***Delete checked
+  $('#delete input[type="submit"').click(function(){
+    if ($('input[type="checkbox"]').prop('checked') == true) {
+      $('#data-size td').hide();
+    }else{
+      false;
+    }
+  });//end click Delete checked
 });//end ready
