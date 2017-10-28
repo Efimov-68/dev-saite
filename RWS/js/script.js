033ccbda521daffa3a9148abdcd77eddb1bf699f
Efimov-68 <r.efimov@list.ru>
2017-10-28 20:22:14 +0300

    ffdf

--- RWS/js/script.js
+++ RWS/js/script.js
@@ -1,25 +1,37 @@
 $(document).ready(function(){
+    // SLIDER
     $('.slider_2').hide();
     $('.slider_3').hide();
     $('.slider_1').show(function(){
-        $('.sub1').addClass('.slider_active');
+        $('.sub1').addClass('slider_active');
     });
+    // LIFE Slider function()
+    //$(window).load(function() {
+    //    $('.slider_1').css();
+    //});
+    // INPUT click()
     $('.sub2').click(function(){
+        $('.slider_2').fadeIn(10);
         $('.slider_1').hide();
         $('.slider_3').hide();
-        $('.slider_2').fadeIn(10);
-        $('.sub2').addClass('.slider_active');
+        $('.sub2').addClass('slider_active');
+        $('.sub1').removeClass('slider_active');
+        $('.sub3').removeClass('slider_active');
     });
     $('.sub3').click(function(){
-        $('.slider_1').hide();
-        $('.slider_2').hide()
         $('.slider_3').fadeIn(10);
-        $('.sub3').addClass('.slider_active');
+        $('.slider_1').hide();
+        $('.slider_2').hide();
+        $('.sub3').addClass('slider_active');
+        $('.sub1').removeClass('slider_active');
+        $('.sub2').removeClass('slider_active');
     });
     $('.sub1').click(function(){
-        $('.slider_3').hide();
-        $('.slider_2').hide()
         $('.slider_1').fadeIn(10);
-        $('.sub1').addClass('.slider_active');
+        $('.slider_3').hide();
+        $('.slider_2').hide();
+        $('.sub1').addClass('slider_active');
+        $('.sub2').removeClass('slider_active');
+        $('.sub3').removeClass('slider_active');
     });
 });
