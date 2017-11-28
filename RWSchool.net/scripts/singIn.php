80520765f01e203adcc39abac06f8b9b69585851
Efimov-68 <r.efimov@list.ru>
2017-11-28 12:59:40 +0300

    rws.net + php files reg + singIn

---
+++ RWSchool.net/scripts/singIn.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+      <meta charset="utf-8"/>
+      <meta name="text/html; charset=UTF-8" http-equiv="Content-Type"/>
+      <meta name="description" content=""/>
+      <meta name="keywords" content=""/>
+      <meta name="viewport" content="width=device-width; initial-scale=1.0"/>
+       <link rel="stylesheet" type="text/css" href="../css/style.css"/>
+       <script src="../js/jquery-3.2.1.min.js"></script>
+       <script src="../js/jquery-ui.min.js"></script>
+       <script src="../js/script.js"></script>
+       <title>SING_IN</title>
+    </head>
+    <body>
+        <div></div>
+    </body>
+</html>
