ab3d894cf420f2e8a601fc01c7a6a692cc2d5a61
Efimov-68 <r.efimov@list.ru>
2017-11-29 14:01:47 +0300

    psw.net singIn cancel work

--- RWSchool.net/js/script.js
+++ RWSchool.net/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         });
     });
 });//END WELCOME
-//REGISTRATION FORM
+//REGISTRATION FORM NEW_USERS
 $(document).ready(function(){
     $('.newUser').hide();
     $('#registration').click(function(){
@@ -17,9 +17,10 @@ $(document).ready(function(){
         $('.newUser_h').html('<h1>Форма регистрации</h1>');
         $('.newUser').css({
             'position': 'absolute',
-            'top': '10%',
+            'top': '0',
             'left': '30%',
             'right': '20%',
+            //'bottom': '0',
             'padding':'20px',
             'border-radius': '5px',
             //'box-shadow': '0 0 10px black',
@@ -32,8 +33,7 @@ $(document).ready(function(){
         //***
         $('.basic_newUser').html('<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis quis neque, doloribus officia distinctio placeat. Ullam reiciendis nam, expedita reprehenderit quam in rerum veniam. Ratione autem reiciendis reprehenderit esse facere. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis quis neque, doloribus officia distinctio placeat. Ullam reiciendis nam, expedita reprehenderit quam in rerum veniam. Ratione autem reiciendis reprehenderit esse facere.</p>');
         //***
-        $('.newUser_regForm').html('<div><p>Имя: <input name="first_name" type="first_name" title="Имя"/></p><p>Фамилия: <input name="last_name" type="last_name" title="Фамилия"/></p><p>Отчество: <input name="middle_name" type="middle_name" title="Отчество"/></p><p>Дата рождения: <input name="birthday" type="date" title="Дата рождения"/></p><p>E-mail: <input name="email" type="email" title="E-mail"/></p></div><p class="check"><input type="checkbox" checked/>Нажимая кнопку "Отправить" я подтвеждаю то, что ознакомлен(а) с условия о предоставлении <a href="../doc/">Персональных данных и их обработки</a></p><p><input type="button" value="Отмена" class="submit_exit"/></p><p><input type="button" value="Отправить" class="reg_submit"/></p>');
-        //
+        $('.newUser_regForm').html('<div><p>Имя: <input name="first_name" type="first_name" title="Имя" placeholder="Имя"/></p><p>Фамилия: <input name="last_name" type="last_name" title="Фамилия" placeholder="Фамилия"/></p><p>Отчество: <input name="middle_name" type="middle_name" title="Отчество" placeholder="Отчество"/></p><p>Дата рождения: <input name="birthday" type="date" title="Дата рождения"/></p><p>E-mail: <input name="email" type="email" title="E-mail" placeholder="E-mail"/></p><p align="center" style="padding:20px 0 0 0;">Придумайте пароль для входа (мин.6 знаков)</p><p>Пароль: <input type="password" name="password" title="Придумайте пароль" placeholder="Пароль"/></p><p>Повторите: <input type="password" name="return_password" title="Повторите пароль" placeholder="Повторите пароль"/></p></div>');
         $('.newUser_regForm div').css({
             'width': '350px',
             'margin': '0 auto'
@@ -44,6 +44,11 @@ $(document).ready(function(){
             'padding-left': '5px',
             'float': 'right'
         });
+        //***
+        $('.newUser_submit').html('<p class="check"><input type="checkbox" checked/>Нажимая кнопку "Отправить" я подтвеждаю то, что ознакомлен(а) с условия о предоставлении <a href="../doc/">Персональных данных и их обработки</a></p><p><input type="button" value="Отмена" class="submit_exit"/></p><p><input type="button" value="Отправить" class="reg_submit"/></p>');
+        $('.newUser_submit input').css({'border': '1px solid lightgray',
+            'height': '30px',
+            'padding-left': '5px'});
         $('.check').css('font-size', '.7em');
         $('.check input').css({
             'float': 'left'
@@ -62,39 +67,67 @@ $(document).ready(function(){
             'background-color': 'ghostwhite',
             'float': 'left'
         });
-        //***
         $('.submit_exit').click(function(){
             $('.newUser').fadeOut(600);
             $('html').css({'background-color': 'white', 'color': 'black'});
         });
     });
-});//END REGISTRATION FORM
+});//END REGISTRATION FORM NEW_USERS
+//SING_IN FORM
 $(document).ready(function(){
     $('#singIn').click(function(){
         $('.singIn_open').fadeIn(600);
         $('html').css({'background-color': 'rgba(215,215,215,.9)', 'color': 'gray'});
         $('.singIn_open').css({
             'position': 'absolute',
-            'top': '10%',
-            'left': '40%',
+            'top': '0',
+            'left': 'auto',
+            'right': '0',
             'padding': '20px',
-            'width': '400px',
+            'width': '300px',
             'border': 'none',
+            'border-radius': '0 0 0 10px',
             'color': 'black',
             'background-color': 'whitesmoke'
         });
-        $('.singIn_h').html('<h1>Войти в систему</h1>');
+        $('.singIn_h').html('<h2>Войти в систему</h2>');
         $('.singIn_h').css({'text-align': 'center'});
-        $('.singIn_basic').html('<p><input name="email" type="text" title="E-mail"/></p><p><input name="password" type="password" title="Пароль"/></p>');
-        $('.singIn_basic input').css({
-            'border': '1px solid lightgray',
-            'padding-left': '10px'
+        $('.singIn_basic').html('<div class=""><p><input name="email" type="text" title="E-mail" placeholder="E-mail"/></p><p><input name="password" type="password" title="Пароль" placeholder="Пароль"/></p></div>');
+        $('.singIn_basic div').css({
+            'margin': '0 auto',
+            'width': '200px'
         });
-        $('.singIn_social').html('<p><img src="./images/social/goog.png" alt="G+"/><img src="./images/social/fb.png" alt="Fb"/><img src="./images/social/vk.png" alt="Vk"/></p>');
-        $('.singIn_social').css('margin', '0 auto');
+        $('.singIn_basic input').css({
+            'width': '200px',
+            'height': '23px',
+            'border': '1px solid lightgray',
+            'padding-left': '0',
+            'text-align': 'center'
+        });
+        $('.singIn_social').html('<div class="vidjets"><p><img src="./images/social/goog.png" alt="G+"/><img src="./images/social/fb.png" alt="Fb"/><img src="./images/social/vk.png" alt="Vk"/></p></div>');
+        $('.singIn_social div').css('margin', '0 auto');
         $('.singIn_social img').css({
             'width': '30px',
             'padding': '5px'
         });
+        $('.singIn_submit').html('<div class="exit"><input type="button" value="Отмена"/></div><div class="open"><input type="button" value="Войти"/></div>');
+        $('.exit input').css({
+            'float': 'left',
+            'padding': '5px 10px',
+            'border': '1px solid lightgray',
+            'border-radius': '0 10px',
+            'backgrounf-color': 'ghostwhite'
+        });
+        $('.open input').css({
+            'float': 'right',
+            'padding': '5px 10px',
+            'border': '1px solid lightgray',
+            'border-radius': '10px 0',
+            'backgrounf-color': 'ghostwhite'
+        });
+        $('.exit input').click(function(){
+            $('.singIn_open').fadeOut(600);
+            $('html').css({'background-color': 'white', 'color': 'black'});
+        });
     });
-});
+});//END SING_IN FORM
