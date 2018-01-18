a22c8757ac26a7b88d1e0c90805eb5297ad35ea9
Efimov-68 <r.efimov@list.ru>
2018-01-18 15:22:14 +0300

    New_site 180118 15:22

--- New_site/new_script.js
+++ New_site/new_script.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
-	var $childrenButton = $("input[value='для Детей']");
-    $childrenButton.load("children00.html");
+	//var $childrenButton = $("input[value='для Детей']");
+  //  $childrenButton.load("children00.html");
+	//
+  $('.right-heading-registration').html('<h2><input type="button" value="+"/>Регистрация на сайте</h2>');
+	$('.right-block-registration').hide();
+	$('.right-heading-singIn').html('<h2><input type="button" value="+"/>Вход на сайт</h2>');
+	$('.right-block-singIn').hide();
+	/*
+	$('.right-heading-registration input[value="+"]').click(function(){
+		$('.right-heading-registration').html('<h2><input type="button" value="-"/>Регистрация на сайте</h2>');
+		$('.right-block-registration').fadeIn();
 	});
+	$('.right-heading-registration input[value="-"]').click(function(){
+		$('.right-heading-registration').html('<h2><input type="button" value="+"/>Регистрация на сайте</h2>');
+		$('.right-block-registration').fadeOut();
+	});
+	*/
+	//
+
+	/*
+
+	if ($('.right-heading-singIn h2 input[value="+"]')) === '+'{
+		$('.right-heading-singIn input[value="+"]').click(function(){
+			$('.right-heading-singIn').html('<h2><input type="button" value="-"/>Вход на сайт</h2>');
+			$('.right-block-singIn').fadeIn();
+		});
+	}else if ($('.right-heading-singIn h2 input[value="-"]')) === '-'{
+		$('.right-heading-singIn input[value="-"]').click(function(){
+			$('.right-heading-singIn').html('<h2><input type="button" value="+"/>Вход на сайт</h2>');
+			$('.right-block-singIn').fadeOut();
+		});
+	}
+	
+	*/
 });//ready()
