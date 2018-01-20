62b48e9034385abbd5972d19295bb1ee5265ecee
Efimov-68 <r.efimov@list.ru>
2018-01-20 17:23:19 +0300

    New_site 19012018 None

--- New_site/new_script.js
+++ New_site/new_script.js
@@ -1,36 +1,25 @@
 $(document).ready(function(){
-	//var $childrenButton = $("input[value='для Детей']");
-  //  $childrenButton.load("children00.html");
 	//
   $('.right-heading-registration').html('<h2><input type="button" value="+"/>Регистрация на сайте</h2>');
 	$('.right-block-registration').hide();
 	$('.right-heading-singIn').html('<h2><input type="button" value="+"/>Вход на сайт</h2>');
 	$('.right-block-singIn').hide();
-	/*
-	$('.right-heading-registration input[value="+"]').click(function(){
-		$('.right-heading-registration').html('<h2><input type="button" value="-"/>Регистрация на сайте</h2>');
-		$('.right-block-registration').fadeIn();
+  $('.right-heading-registration input').click(function(){
+	  if ($('.right-heading-registration input[value="+"]') === $('.right-heading-registration input[value="+"]')){
+		  $('.right-heading-registration').html('<h2><input type="button" value="-"/>Вход на сайт</h2>');
+		  $('.right-block-registration').fadeIn();
+	  }else if ($('.right-heading-registration input[value="-"]') === $('.right-heading-registration input[value="-"]')){
+			$('.right-heading-registration').html('<h2><input type="button" value="+"/>Вход на сайт</h2>');
+			$('.right-block-registration').fadeOut();
+	  }
 	});
-	$('.right-heading-registration input[value="-"]').click(function(){
-		$('.right-heading-registration').html('<h2><input type="button" value="+"/>Регистрация на сайте</h2>');
-		$('.right-block-registration').fadeOut();
-	});
-	*/
-	//
-
-	/*
-
-	if ($('.right-heading-singIn h2 input[value="+"]')) === '+'{
-		$('.right-heading-singIn input[value="+"]').click(function(){
-			$('.right-heading-singIn').html('<h2><input type="button" value="-"/>Вход на сайт</h2>');
-			$('.right-block-singIn').fadeIn();
-		});
-	}else if ($('.right-heading-singIn h2 input[value="-"]')) === '-'{
-		$('.right-heading-singIn input[value="-"]').click(function(){
+  $('.right-heading-singIn input').click(function(){
+	  if ($('.right-heading-singIn input[value="+"]') === $('.right-heading-singIn input[value="+"]')){
+		  $('.right-heading-singIn').html('<h2><input type="button" value="-"/>Вход на сайт</h2>');
+		  $('.right-block-singIn').fadeIn();
+	  }else if ($('.right-heading-singIn input[value="-"]') === $('.right-heading-singIn input[value="-"]')){
 			$('.right-heading-singIn').html('<h2><input type="button" value="+"/>Вход на сайт</h2>');
 			$('.right-block-singIn').fadeOut();
-		});
-	}
-	
-	*/
+	  }
+	});//end registration && singIn
 });//ready()
