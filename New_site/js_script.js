c3f375fda2346768fb29e4b6885971a599e934c3
Efimov-68 <r.efimov@list.ru>
2018-01-23 13:36:07 +0300

    New_site 230118 13:36

---
+++ New_site/js_script.js
@@ -0,0 +1,28 @@
+var slider = {
+  //массив с изображением
+  slides:['6.jpg', '9jpg', '20jpg'],
+  //начальный кадр (индекс из массива, нумерация с 0)
+  frame: 0,
+  set: function(image) { //установка нужного фона слайдеру
+    document.getElementById("scr").style.backgroundImage = "url('+image+')";
+  },
+  init: function() { //запуск слайдера с начальной картинкой
+    this.set(this.slides[this.frame]);
+  },
+  left: function() { //влево
+    this.frame--;
+    if(this.frame < 0) this.frame = this.slides.length-1;
+    this.set(this.slides[this.frame]);
+  },
+  right: function() { //вправо
+    this.frame++;
+    if(this.frame == this.slides.length) this.frame = 0;
+    this.set(this.slides[this.frame]);
+  }
+};
+window.onload = function() { //запуск слайдера после загрузки
+  slider.init();
+  setInterval(function() { //5 секунд
+    slider.right(); //после - переход на кадр справа
+  }, 5000);
+};
